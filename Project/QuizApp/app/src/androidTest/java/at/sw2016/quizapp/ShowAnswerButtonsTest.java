0f459b4a178691e33aec64112444268a0c1d34fd
Lukas Radacher <l.radacher@gmail.com>
2016-05-06 23:13:37 +0200

    1.7 create database with working schema and necessary dao etc. test not commited as not working properly

--- Project/QuizApp/app/src/androidTest/java/at/sw2016/quizapp/ShowAnswerButtonsTest.java
+++ Project/QuizApp/app/src/androidTest/java/at/sw2016/quizapp/ShowAnswerButtonsTest.java
@@ -2,6 +2,7 @@ package at.sw2016.quizapp;
 
 import android.graphics.drawable.ColorDrawable;
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.UiThreadTest;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -25,26 +26,23 @@ public class ShowAnswerButtonsTest extends ActivityInstrumentationTestCase2 {
         mySolo = new Solo(getInstrumentation(), getActivity());
     }
 
-    public void testQuiz() {
-        testButtons(R.id.lower_left_button, "42");
-        testButtons(R.id.lower_right_button, "2.718");
-        testButtons(R.id.upper_left_button, "3.141");
-        testButtons(R.id.upper_right_button, "36");
+    @UiThreadTest
+    public void testLayout() {
+        buttonsTest(R.id.lower_left_button, "42");
+        buttonsTest(R.id.lower_right_button, "2.718");
+        buttonsTest(R.id.upper_left_button, "3.141");
+        buttonsTest(R.id.upper_right_button, "36");
     }
 
-    public void testButtons(int id, String buttonText) {
+    public void buttonsTest(int id, String buttonText) {
         final Button button = (Button) mySolo.getCurrentActivity().findViewById(id);
         assertEquals(View.VISIBLE, button.getVisibility());
         assertEquals(buttonText , button.getText().toString());
-        getActivity().runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
                 button.performClick();
                 ColorDrawable buttonColor = (ColorDrawable) button.getBackground();
                 if( buttonColor != null){
                     assertEquals(buttonColor.getColor(), 0xFFB74D);
                 }
-            }
-        });
+
     }
 }
--- Project/QuizApp/app/src/main/java/at/sw2016/quizapp/MainActivity.java
+++ Project/QuizApp/app/src/main/java/at/sw2016/quizapp/MainActivity.java
@@ -4,14 +4,19 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import at.sw2016.quizapp.database.DatabaseHelper;
+
 public class MainActivity extends AppCompatActivity {
 
+    DatabaseHelper databaseHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
         setContentView(R.layout.activity_main);
 
+        databaseHelper = new DatabaseHelper(getApplicationContext());
         Intent intent = new Intent(this, TrainingActivity.class);
         startActivity(intent);
     }
