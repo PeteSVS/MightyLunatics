8beaeff125acb989c5cb52657fef64c52040cce4
Lukas Radacher <l.radacher@gmail.com>
2016-05-10 00:09:32 +0200

    1.10 [PP,LR] get a random question from table

---
+++ Project/QuizApp/app/src/androidTest/java/at/sw2016/quizapp/DatabaseTest.java
@@ -0,0 +1,67 @@
+package at.sw2016.quizapp;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.AndroidTestCase;
+import android.test.RenamingDelegatingContext;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.widget.TextView;
+
+import com.robotium.solo.Solo;
+
+import at.sw2016.quizapp.database.QuestionDao;
+import at.sw2016.quizapp.model.Question;
+import at.sw2016.quizapp.utils.Category;
+
+/**
+ * Created by Lukas on 05.05.2016.
+ */
+public class DatabaseTest extends AndroidTestCase {
+
+    private QuestionDao questionDao;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        RenamingDelegatingContext context = new RenamingDelegatingContext(getContext(), "test_");
+        questionDao = new QuestionDao(context);
+        questionDao.open();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        questionDao.close();
+        super.tearDown();
+    }
+
+    @LargeTest
+    public void testDatabaseConnection() throws Exception {
+        Question testQuestion = new Question();
+        testQuestion.setQuestion("Welche Antwort ist richtig?");
+        testQuestion.setAnswer1("1");
+        testQuestion.setAnswer2("2");
+        testQuestion.setAnswer3("3");
+        testQuestion.setAnswer4("4");
+        testQuestion.setCorrectAnswer("1");
+        testQuestion.setCategory(Category.HISTORY);
+        long id = questionDao.addQuestion(testQuestion);
+        Question answerQuestion = questionDao.getQuestion(id);
+        assertEquals(answerQuestion.getQuestion(), testQuestion.getQuestion());
+    }
+
+    @LargeTest
+    public void testRandomQuestion() throws Exception {
+        Question testQuestion = new Question();
+        testQuestion.setQuestion("Welche Antwort ist richtig?");
+        testQuestion.setAnswer1("1");
+        testQuestion.setAnswer2("2");
+        testQuestion.setAnswer3("3");
+        testQuestion.setAnswer4("4");
+        testQuestion.setCorrectAnswer("1");
+        testQuestion.setCategory(Category.HISTORY);
+        long id = questionDao.addQuestion(testQuestion);
+        Question answerQuestion = questionDao.getRandomQuestion();
+        assertEquals(answerQuestion.getQuestion(), testQuestion.getQuestion());
+    }
+}
---
+++ Project/QuizApp/app/src/main/java/at/sw2016/quizapp/database/QuestionHelper.java
@@ -0,0 +1,66 @@
+package at.sw2016.quizapp.database;
+
+import android.provider.BaseColumns;
+
+import at.sw2016.quizapp.model.Question;
+
+/**
+ * Created by Lukas on 05.05.2016.
+ */
+public class QuestionHelper {
+
+    public static abstract class QuestionEntry implements BaseColumns {
+        public static final String TABLE_NAME = "questions";
+        public static final String COLUMN_NAME_ENTRY_ID = "id";
+        public static final String COLUMN_NAME_QUESTION = "question";
+        public static final String COLUMN_NAME_CORRECT_ANSWER = "answercorrect";
+        public static final String COLUMN_NAME_ANSWER_2 = "answer2";
+        public static final String COLUMN_NAME_ANSWER_3 = "answer3";
+        public static final String COLUMN_NAME_ANSWER_4 = "answer4";
+        public static final String COLUMN_NAME_CATEGORY = "category";
+    }
+
+    private static final String TEXT_TYPE = "TEXT";
+    private static final String INTEGER_TYPE = "INTEGER";
+    private static final String EMPTY_SPACE = " ";
+    private static final String COMMA_SEP = ",";
+
+    private static final String CREATE_QUESTION_ENTRY = "CREATE TABLE "
+            + QuestionEntry.TABLE_NAME + "(" + QuestionEntry.COLUMN_NAME_ENTRY_ID
+            + " INTEGER PRIMARY KEY,"
+            + QuestionEntry.COLUMN_NAME_QUESTION + EMPTY_SPACE + TEXT_TYPE + COMMA_SEP
+            + QuestionEntry.COLUMN_NAME_CORRECT_ANSWER + EMPTY_SPACE + TEXT_TYPE + COMMA_SEP
+            + QuestionEntry.COLUMN_NAME_ANSWER_2 + EMPTY_SPACE + TEXT_TYPE + COMMA_SEP
+            + QuestionEntry.COLUMN_NAME_ANSWER_3 + EMPTY_SPACE + TEXT_TYPE + COMMA_SEP
+            + QuestionEntry.COLUMN_NAME_ANSWER_4 + EMPTY_SPACE + TEXT_TYPE + COMMA_SEP
+            + QuestionEntry.COLUMN_NAME_CATEGORY + EMPTY_SPACE + INTEGER_TYPE + ")";
+
+    private static final String DELETE_QUESTION_ENTRIES =
+            "DROP TABLE IF EXISTS " + QuestionEntry.TABLE_NAME;
+
+    private static final String DELETE_QUESTION_ENTRY = QuestionEntry.COLUMN_NAME_ENTRY_ID + " = ?";
+
+    private static final String SELECT_QUESTION_BY_ID = "SELECT * FROM " + QuestionEntry.TABLE_NAME + " WHERE " + QuestionEntry.COLUMN_NAME_ENTRY_ID + " = ";
+
+    private static final String SELECT_RANDOM_QUESTION = "SELECT * FROM " + QuestionEntry.TABLE_NAME + " ORDER BY RANDOM() LIMIT 1;";
+
+    public static String getCreateQuestionEntry() {
+        return CREATE_QUESTION_ENTRY;
+    }
+
+    public static String getDeleteQuestionEntries(){
+        return DELETE_QUESTION_ENTRIES;
+    }
+
+    public static String getDeleteQuestionEntry() {
+        return DELETE_QUESTION_ENTRY ;
+    }
+
+    public static String getSelectQuestionById(long id) {
+        return SELECT_QUESTION_BY_ID + id;
+    }
+
+    public static String getSelectRandomQuestion() {
+        return SELECT_RANDOM_QUESTION;
+    }
+}
