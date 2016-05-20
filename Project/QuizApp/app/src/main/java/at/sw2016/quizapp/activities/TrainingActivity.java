2896543ae13698c7bc999567c89ae4ada85b7750
Lukas Radacher <l.radacher@gmail.com>
2016-05-20 18:40:06 +0200

    [LR, PP] 1.12 almost finished

--- Project/QuizApp/app/src/main/java/at/sw2016/quizapp/activities/TrainingActivity.java
+++ Project/QuizApp/app/src/main/java/at/sw2016/quizapp/activities/TrainingActivity.java
@@ -1,6 +1,7 @@
 package at.sw2016.quizapp.activities;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -13,24 +14,29 @@ import at.sw2016.quizapp.model.Question;
 
 public class TrainingActivity extends AppCompatActivity {
 
+    private QuizApplication quizApplication;
+    private Question currentQuestion;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_training);
+        quizApplication = (QuizApplication) getApplicationContext();
+        createQuestion();
+    }
 
-        final QuizApplication application = (QuizApplication) getApplicationContext();
-        Question question = application.getQuestionDao().getRandomQuestion();
+    public void createQuestion() {
+        currentQuestion = getQuizApplication().getQuestionDao().getRandomQuestion();
 
         // globally
         TextView questionTextView = (TextView)findViewById(R.id.question_field);
         if (questionTextView != null) {
-            questionTextView.setText(question.getQuestion());
+            questionTextView.setText(getCurrentQuestion().getQuestion());
         }
-        createButton(R.id.upper_left_button, question.getAnswer1());
-        createButton(R.id.upper_right_button, question.getAnswer2());
-        createButton(R.id.lower_left_button, question.getAnswer3());
-        createButton(R.id.lower_right_button, question.getAnswer4());
+        createButton(R.id.upper_left_button, getCurrentQuestion().getAnswer1());
+        createButton(R.id.upper_right_button, getCurrentQuestion().getAnswer2());
+        createButton(R.id.lower_left_button, getCurrentQuestion().getAnswer3());
+        createButton(R.id.lower_right_button, getCurrentQuestion().getAnswer4());
     }
 
     public void createButton(int id, String text){
@@ -40,6 +46,7 @@ public class TrainingActivity extends AppCompatActivity {
             button.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
                     changeColor((Button) v);
+                    validateAnswer((Button) v);
                 }
             });
         }
@@ -47,5 +54,76 @@ public class TrainingActivity extends AppCompatActivity {
 
     public void changeColor(Button button){
         button.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.buttonFeedbackColor));
+    }
+
+    public void validateAnswer(final Button button){
+        if( button.getText() == null)
+            return;
+
+        if(button.getText().equals(currentQuestion.getCorrectAnswer())){
+            styleCorrectChoosenButton(button);
+        } else {
+            Button correctButton = findCorrectButton(button);
+            styleWrongChoosenButton(correctButton, button);
+        }
+    }
+
+    protected void styleCorrectChoosenButton(final Button button){
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                button.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.buttonCorrectAnswer));
+            }
+        }, 1000);
+    }
+
+    protected void styleWrongChoosenButton(final Button correctButton, final Button clickedButton){
+        if(correctButton != null ){
+            final Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    clickedButton.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.buttonWrongAnswer));
+                    correctButton.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.buttonCorrectAnswer));
+                }
+            }, 1000);
+        }
+    }
+
+    protected Button findCorrectButton(Button clickedButton){
+        if(isCorrectButton(R.id.upper_left_button)){
+            return (Button) findViewById(R.id.upper_left_button);
+        }
+        if(isCorrectButton(R.id.upper_right_button)){
+            return (Button) findViewById(R.id.upper_right_button);
+        }
+        if(isCorrectButton(R.id.lower_left_button)){
+            return (Button) findViewById(R.id.lower_left_button);
+        }
+        if(isCorrectButton(R.id.lower_right_button)){
+            return (Button) findViewById(R.id.lower_right_button);
+        }
+        return null;
+    }
+
+    protected boolean isCorrectButton(int id){
+        final Button correctButton = (Button) findViewById(id);
+        if(correctButton != null && correctButton.getText() != null && correctButton.getText().equals(currentQuestion.getCorrectAnswer())){
+            return true;
+        }
+        return false;
+    }
+
+    public QuizApplication getQuizApplication() {
+        return quizApplication;
+    }
+
+    public Question getCurrentQuestion() {
+        return currentQuestion;
+    }
+
+    public void setCurrentQuestion(Question currentQuestion) {
+        this.currentQuestion = currentQuestion;
     }
 }
