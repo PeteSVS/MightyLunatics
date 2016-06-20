fe0a253252b78957890160bf8198da132267c428
Manuel Papst <dope696@gmail.com>
2016-06-20 11:44:34 +0200

    [MP, SG] 2.5 added show Score

---
+++ Project/QuizApp/app/src/main/java/at/sw2016/quizapp/activities/HighscoreActivity.java
@@ -0,0 +1,18 @@
+package at.sw2016.quizapp.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import at.sw2016.quizapp.R;
+
+public class HighscoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.highscore);
+    }
+
+}
+
+
